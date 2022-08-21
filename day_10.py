7c31f7806c4c1354bb19e271efb86b109f21192c
Peabody29 <joshampofo92@gmail.com>
2022-08-21 20:28:19 +0000

    Update day_10.py

--- day_10.py
+++ day_10.py
@@ -2,9 +2,20 @@
 from a user and hides the password, returning the length of the input to the user"""
 
 
-def hide_password():
-    user = input('enter your name: ')
-    return f"Your password is {len(user) * '*'} and it's {len(user)} characters long"
+# def hide_password():
+#     user = input('enter your name: ')
+#     return f"Your password is {len(user) * '*'} and it's {len(user)} characters long"
+#
+#
+# print(hide_password())
+
+#####################################################################
+def convert_numbers(num):
+    new_list = []
+    for num in num:
+        new_list.append('{:,}'.format(num))
+    return new_list
 
 
-print(hide_password())
+print(convert_numbers([1000000, 2356989, 2354672, 9878098]))
+
