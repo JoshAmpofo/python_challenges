7cbbc172e4749c6a455dfa01aec3517f992cbb6a
Peabody29 <78964254+Peabody29@users.noreply.github.com>
2022-08-21 19:46:01 +0000

    Add files via upload
    challenge number 7

---
+++ day_7.py
@@ -0,0 +1,12 @@
+"""This challenge involves creating a range out of a single value and returning the range as a string
+separated by dots"""
+
+
+def string_range(num):
+    x = [str(n) for n in range(num)]  # use list comprehension to create range
+    x = '.'.join(x)  # use join method to separate values with a dot
+    return x
+
+
+print(string_range(6))
+
